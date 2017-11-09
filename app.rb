93fb7a87a311031514cbc4c652b8b971d81b29a1
Hannah Danziger <hdanziger8@gmail.com>
2017-11-09 19:24:21 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -19,12 +19,13 @@ class App < Sinatra::Base
     "#{@phrase * @number}"
   end
 
-  get get '/say/:word1/:word2/:word3/:word4/:word5' do 
+  get get '/say/:word1/:word2/:word3/:word4/:word5' do
     @word1 = params[:word1]
     @word2 = params[:word2]
     @word3 = params[:word3]
     @word4 = params[:word4]
     @word5 = params[:word5]
     "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}"
+  end
 
 end
