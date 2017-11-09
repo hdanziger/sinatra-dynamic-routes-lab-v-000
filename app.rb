c1e3c96805cb8f33b19b6dbd098c06b0fb52d0e0
Hannah Danziger <hdanziger8@gmail.com>
2017-11-09 19:30:29 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -28,4 +28,19 @@ class App < Sinatra::Base
     "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
   end
 
+  get '/:operation/:number1/:number2' do
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
+
+    case params
+    when "add"
+      answer = (@number1 + @number2).to_s
+    when "subract"
+      answer = (@number1 - @number2).to_s
+    when "multiply"
+      answer = (@number1 * @number2).to_s
+    when "divide"
+      answer = (@number1 % @number2).to_s
+  end
+
 end
